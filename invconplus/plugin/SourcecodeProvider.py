d10c62b770c6edd9ecd47802132994b077343ee8
Angela Novelli <ange.novelli@studenti.unina.it>
2026-05-07 00:09:43 +0200

    Fix indentation in crawl method for Slither initialization

--- invconplus/plugin/SourcecodeProvider.py
+++ invconplus/plugin/SourcecodeProvider.py
@@ -23,7 +23,7 @@ class SourcecodeProvider(Provider):
         
     def crawl(self):
         try:
-          slither = Slither(target="mainet:{0}".format(self.params[CONTRACT_ADDRESS]), export_dir = "./crytic-export", etherscan_api_key=os.environ.get("ETHERSCAN_API_KEY", ""))
+            slither = Slither(target="mainet:{0}".format(self.params[CONTRACT_ADDRESS]), export_dir = "./crytic-export", etherscan_api_key=os.environ.get("ETHERSCAN_API_KEY", ""))
             # print(slither._crytic_compile.filenames)
             if len(slither._crytic_compile.filenames) == 1:
                 mainContract = list(slither._crytic_compile.filenames)[0].relative.split(".etherscan.io-")[1].split(".sol")[0]
